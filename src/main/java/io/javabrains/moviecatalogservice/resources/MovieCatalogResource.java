ab735b3cddd07b2ad8416366cc96bc4bf633a8b6
Aritra1704 <aritra1704@gmail.com>
2021-05-18 19:02:41 +0530

    Integrated Rating api. Added Eureka client setup. Named the micro service.

--- src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,11 +3,17 @@ package io.javabrains.moviecatalogservice.resources;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
+import io.javabrains.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.netflix.discovery.DiscoveryClient;
+
+import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -17,16 +23,29 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+    
+    @Autowired
+    DiscoveryClient discoveryClient;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        RestTemplate restTemplate = new RestTemplate();
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
-        return ratings.stream().map(rating -> {
-            Movie[] movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie[].class);
+        UserRating ratings = restTemplate.getForObject("http://rating-data-service/ratingsdata/user/"+userId,
+                UserRating.class);
+        return ratings.getUserRatings().stream().map(rating -> {
+//            Movie[] movie = webClientBuilder.build()
+//                    .get()
+//                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
+//                    .retrieve()
+//                    .bodyToMono(Movie[].class)
+//                    .block();
+//            return new CatalogItem(movie[0].getName(), "Test", rating.getRating());
+            Movie[] movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie[].class);
             return new CatalogItem(movie[0].getName(), "Test", rating.getRating());
         }).collect(Collectors.toList());
 //        return Collections.singletonList(
